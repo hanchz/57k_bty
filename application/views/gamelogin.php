33bfc25afa703f717f019cd3c04f3328c629d59c
jian03j <jian03j@126.com>
2017-07-21 16:35:15 +0800

    1

--- application/views/gamelogin.php
+++ application/views/gamelogin.php
@@ -9,9 +9,17 @@ function gameurl($gameid,$uid)
 	$serverid=1;
 	$time=time();
 	$key="e2SExYMWng9fMVQS";
-	$gameid="91284";
 	$sign = strtolower(md5($uid.$serverid.$time.$gameid.$key));
 	$url="http://api.egret-labs.org/v2/game/22694/91284";
+	$url=$url."?uid=".$uid."&serverid=".$serverid."&time=".$time."&gameid=".$gameid."&sign=".$sign;	
+	
+	}elseif($gameid=='1')
+	{
+	$serverid=1;
+	$time=time();
+	$key="NvDgEAGoH99CFVPi";
+	$sign = strtolower(md5($uid.$serverid.$time.$gameid.$key));
+	$url="http://119.23.104.2/app/vs4/";
 	$url=$url."?uid=".$uid."&serverid=".$serverid."&time=".$time."&gameid=".$gameid."&sign=".$sign;	
 	
 	}else{$url="";}
