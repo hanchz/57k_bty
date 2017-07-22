0150a1408779e3f5463960b6d2531edab8ad8def
hanchz <han_ds81@hotmail.com>
2017-07-22 17:35:52 +0800

    x

--- application_admin/core/MY_Service.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class MY_Service{
-    function __get($key){
-        $CI = & get_instance();
-        return $CI->$key;
-    }
-}
